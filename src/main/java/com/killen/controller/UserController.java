6524e738d06661a863f81abdc31a01cfb2dbda1f
killence <1654401766@qq.com>
2020-09-01 10:49:48 +0800

    删除

--- src/main/java/com/killen/controller/UserController.java
+++ src/main/java/com/killen/controller/UserController.java
@@ -14,9 +14,4 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class UserController {
 
-    @RequestMapping("/hello")
-    public String hello() {
-        return "hello";
-    }
-
 }
